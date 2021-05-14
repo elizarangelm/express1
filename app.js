ef43d9a0d199bc37a947b2f4a84317bb7f4ce9c9
Elizabeth <elizarangelm24@gmail.com>
2021-05-13 19:26:17 -0500

    saludame 2

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    let nombre;
-    if(req.query.nombre){
-        nombre=req.query.nombre;
-    }
-    else{
-        nombre="desconocido"
-    }
-  res.send('<h1>Hola '+nombre+'!</h1>');
+app.get('/makers/:name', (req, res) => {  
+
+    
+      const capitaliceWord = capitalizar(req.params.name);
+      res.send(`<h1>Hola ${capitaliceWord}!</h1>`);
+  
 });
+
+function capitalizar(str) {    
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();  
+}
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
